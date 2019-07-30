23d127380cb8b14e3ac8afc0f03e23436051b1b3
Jack Kinsey <jack@hexbitstudios.com>
2019-07-30 18:36:52 -0500

    Adds cart item data to the app store in vuex

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import items from './data/items.json';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    items,
+  },
   mutations: {},
   actions: {},
 });
