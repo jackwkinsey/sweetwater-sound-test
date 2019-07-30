927333da7a6a4006764fab6603df6c271549cdd1
Jack Kinsey <jack@hexbitstudios.com>
2019-07-30 18:50:47 -0500

    Adds computed cart property that gets the cart from vuex state; lists item images in cart.

--- src/cart/Cart.vue
+++ src/cart/Cart.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <h1>Your Cart</h1>
+    <div class="item" v-for="(item, index) in cart" :key="index">
+      <div class="image">
+        <img :src="item.image" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'Cart',
+  computed: {
+    cart() {
+      return this.$store.state.cart;
+    },
+  },
+};
 </script>
 
 <style></style>
