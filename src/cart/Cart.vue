054e0257df3e38adeb2b617ecb9eed9aff60be09
Jack Kinsey <jack@hexbitstudios.com>
2019-07-30 19:29:38 -0500

    Displays pricing information for each item in cart and a cart total

--- src/cart/Cart.vue
+++ src/cart/Cart.vue
@@ -12,7 +12,29 @@
         </h3>
         <p class="description">{{ item.description }}</p>
       </div>
+      <div class="item-cost">
+        <table>
+          <thead>
+            <tr>
+              <th>Price</th>
+              <th>Qty</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{{ item.price }}</td>
+              <td>{{ item.quantity }}</td>
+              <td>{{ item.price * item.quantity }}</td>
+            </tr>
+          </tbody>
+        </table>
+        <!-- <span>{{ item.price }}</span>
+        <span>{{ item.quantity }}</span>
+        <span>{{ item.price * item.quantity }}</span>-->
+      </div>
     </div>
+    <div>Total: {{ total }}</div>
   </div>
 </template>
 
@@ -23,6 +45,15 @@ export default {
     cart() {
       return this.$store.state.cart;
     },
+    total() {
+      let total = 0;
+
+      this.cart.forEach(item => {
+        total += item.price * item.quantity;
+      });
+
+      return total;
+    },
   },
 };
 </script>
@@ -31,5 +62,15 @@ export default {
 .item {
   display: flex;
   align-items: center;
+  justify-content: space-between;
+}
+
+.item-image {
+  width: 125px;
+  height: 125px;
+}
+
+.item-details {
+  width: 450px;
 }
 </style>
