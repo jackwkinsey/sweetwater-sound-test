a468c59049de9759625b72bbbe71b5fe3f53ec91
Jack Kinsey <jack@hexbitstudios.com>
2019-07-30 19:42:48 -0500

    Turns cart into a table to see if that makes more sense for the view

--- src/cart/Cart.vue
+++ src/cart/Cart.vue
@@ -1,40 +1,39 @@
 <template>
   <div>
     <h1>Your Cart</h1>
-    <div class="item" v-for="(item, index) in cart" :key="index">
-      <div>
-        <img :src="item.image" class="item-image" />
-      </div>
-      <div class="item-details">
-        <h3 class="name">
-          <a :href="item.url" target="_blank">{{ item.productName }}</a> -
-          <small>{{ item.manufacturer }}</small>
-        </h3>
-        <p class="description">{{ item.description }}</p>
-      </div>
-      <div class="item-cost">
-        <table>
-          <thead>
-            <tr>
-              <th>Price</th>
-              <th>Qty</th>
-              <th>Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{{ item.price }}</td>
-              <td>{{ item.quantity }}</td>
-              <td>{{ item.price * item.quantity }}</td>
-            </tr>
-          </tbody>
-        </table>
-        <!-- <span>{{ item.price }}</span>
-        <span>{{ item.quantity }}</span>
-        <span>{{ item.price * item.quantity }}</span>-->
-      </div>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Description</th>
+            <th>Qty</th>
+            <th>Price</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in cart" :key="index">
+            <td>
+              <img :src="item.image" />
+            </td>
+            <td>
+              <h3 class="name">
+                <a :href="item.url" target="_blank">{{ item.productName }}</a> -
+                <small>{{ item.manufacturer }}</small>
+              </h3>
+              <p class="description">{{ item.description }}</p>
+            </td>
+            <td>{{ item.quantity }}</td>
+            <td>{{ item.price }}</td>
+            <td>{{ item.price * item.quantity }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <div>Total: {{ total }}</div>
+    <div>
+      <strong>Total: {{ total }}</strong>
+    </div>
   </div>
 </template>
 
